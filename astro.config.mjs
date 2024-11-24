905e3f4d3070c20006619f397556acee67ee552d
Ohsem Domain <ohsemdomain@gmail.com>
2024-11-24 11:03:47 +0800

    Changes to be committed: 	modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   public/fonts/Inter-Bold.woff2 	new file:   public/fonts/Inter-Regular.woff2 	new file:   public/fonts/Inter-SemiBold.woff2 	new file:   public/robots.txt 	new file:   src/assets/images/hero-image.jpeg 	new file:   src/components/Icon.astro 	new file:   src/components/base/Footer.astro 	new file:   src/components/base/LayoutWrapper.astro 	new file:   src/components/base/MobileMenu.astro 	new file:   src/components/base/Sidebar.astro 	modified:   src/layouts/Layout.astro 	modified:   src/pages/index.astro 	new file:   src/pages/test.astro 	new file:   src/pages/test2.astro 	new file:   src/styles/global.css 	new file:   tailwind.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,27 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import tailwind from '@astrojs/tailwind';
+
+import partytown from '@astrojs/partytown';
+
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  site: 'https://yourwebsite.com',
+  integrations: [alpinejs(), tailwind(), partytown(), sitemap(), mdx(), icon()]
 });
